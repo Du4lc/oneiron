aecef1d533ddcded864388fb36c42f403bbd41a4
Du4lc <soydu4lc@gmail.com>
2025-08-16 13:31:44 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,33 +1,64 @@
-// Países / Provincias principales (occidentales)
+// === Configuración general ===
+
+// Países disponibles
 export const COUNTRIES = [
-  "España","Portugal","Francia","Italia","Alemania","Reino Unido","Irlanda",
-  "Países Bajos","Bélgica","Suiza","Austria","Suecia","Noruega","Dinamarca","Finlandia",
-  "Estados Unidos","Canadá","Australia"
+  "España",
+  "Portugal",
+  "Francia",
+  "Italia",
+  "Alemania",
+  "Reino Unido",
+  "Estados Unidos",
+  "Canadá"
 ];
 
-export const PROVINCES = {
-  "España": ["Álava","Albacete","Alicante","Almería","Asturias","Ávila","Badajoz","Barcelona","Burgos",
-    "Cáceres","Cádiz","Cantabria","Castellón","Ciudad Real","Córdoba","A Coruña","Cuenca","Girona",
-    "Granada","Guadalajara","Guipúzcoa","Huelva","Huesca","Islas Baleares","Jaén","León","Lleida",
-    "Lugo","Madrid","Málaga","Murcia","Navarra","Ourense","Palencia","Las Palmas","Pontevedra",
-    "La Rioja","Salamanca","Santa Cruz de Tenerife","Segovia","Sevilla","Soria","Tarragona","Teruel",
-    "Toledo","Valencia","Valladolid","Vizcaya","Zamora","Zaragoza","Ceuta","Melilla"
+// Provincias / Estados por país
+export const REGIONS_BY_COUNTRY = {
+  "España": [
+    "Madrid", "Barcelona", "Valencia", "Sevilla", "Málaga", "Zaragoza",
+    "Murcia", "Alicante", "Cádiz", "Granada", "Toledo", "Salamanca",
+    "Valladolid", "Badajoz", "A Coruña", "Asturias", "Cantabria", "Navarra",
+    "La Rioja", "Álava", "Guipúzcoa", "Vizcaya", "Ceuta", "Melilla"
   ],
-  "Portugal": ["Aveiro","Beja","Braga","Bragança","Castelo Branco","Coimbra","Évora","Faro","Guarda","Leiria","Lisboa","Portalegre","Porto","Santarém","Setúbal","Viana do Castelo","Vila Real","Viseu","Madeira","Açores"],
-  "Francia": ["Auvernia-Ródano-Alpes","Borgoña-Franco Condado","Bretaña","Centro-Valle de Loira","Córcega","Gran Este","Hauts-de-France","Isla de Francia","Normandía","Nueva Aquitania","Occitania","Países del Loira","Provenza-Alpes-Costa Azul"],
-  "Italia": ["Abruzos","Basilicata","Calabria","Campania","Emilia-Romaña","Friuli-Venecia Julia","Lacio","Liguria","Lombardía","Las Marcas","Molise","Piamonte","Apulia","Cerdeña","Sicilia","Toscana","Trentino-Alto Adigio","Umbría","Valle de Aosta","Véneto"],
-  "Alemania": ["Baden-Wurtemberg","Baviera","Berlín","Brandeburgo","Bremen","Hamburgo","Hesse","Mecklemburgo-Pomerania Occidental","Baja Sajonia","Renania del Norte-Westfalia","Renania-Palatinado","Sarre","Sajonia","Sajonia-Anhalt","Schleswig-Holstein","Turingia"],
-  "Reino Unido": ["Inglaterra","Escocia","Gales","Irlanda del Norte"],
-  "Irlanda": ["Dublín","Cork","Galway","Limerick","Waterford","Kerry","Kilkenny","Tipperary","Clare","Mayo","Donegal","Sligo","Wexford","Wicklow","Kildare","Meath","Louth","Carlow","Offaly","Westmeath","Longford","Laois","Roscommon","Monaghan","Cavan"],
-  "Países Bajos": ["Drente","Flevolanda","Frisia","Güeldres","Groninga","Holanda Meridional","Holanda Septentrional","Limburgo","Brabante Septentrional","Overijssel","Utrecht","Zelanda"],
-  "Bélgica": ["Amberes","Brabante Flamenco","Limburgo","Flandes Oriental","Flandes Occidental","Bruselas-Capital","Brabante Valón","Henao","Lieja","Luxemburgo","Namur"],
-  "Suiza": ["Zúrich","Berna","Lucerna","Uri","Schwyz","Obwalden","Nidwalden","Glaris","Zug","Friburgo","Soleura","Basilea-Ciudad","Basilea-Campiña","Schaffhausen","Appenzell Externo","Appenzell Interno","San Galo","Grisones","Argovia","Turgovia","Tesino","Vaud","Valais","Neuchâtel","Ginebra","Jura"],
-  "Austria": ["Burgenland","Carintia","Baja Austria","Alta Austria","Salzburgo","Estiria","Tirol","Vorarlberg","Viena"],
-  "Suecia": ["Blekinge","Dalarna","Gotland","Gävleborg","Halland","Jämtland","Jönköping","Kalmar","Kronoberg","Norrbotten","Skåne","Estocolmo","Södermanland","Uppsala","Värmland","Västerbotten","Västernorrland","Västmanland","Västra Götaland","Örebro","Östergötland"],
-  "Noruega": ["Agder","Innlandet","Møre og Romsdal","Nordland","Oslo","Rogaland","Troms og Finnmark","Trøndelag","Vestfold og Telemark","Vestland","Viken"],
-  "Dinamarca": ["Capital","Zelanda","Jutlandia Central","Jutlandia del Norte","Dinamarca Meridional"],
-  "Finlandia": ["Kainuu","Carelia del Norte","Ostrobotnia del Norte","Päijänne Tavastia","Pirkanmaa","Satakunta","Savonia del Norte","Savonia del Sur","Uusimaa","Finlandia Central","Ostrobotnia","Ostrobotnia Central","Kymenlaakso","Carelia del Sur","Lapland","Finlandia del Sudoeste","Etelä-Karjala"],
-  "Estados Unidos": ["Alabama","Alaska","Arizona","Arkansas","California","Carolina del Norte","Carolina del Sur","Colorado","Connecticut","Dakota del Norte","Dakota del Sur","Delaware","Florida","Georgia","Hawái","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Luisiana","Maine","Maryland","Massachusetts","Míchigan","Minnesota","Misisipi","Misuri","Montana","Nebraska","Nevada","Nueva Hampshire","Nueva Jersey","Nuevo México","Nueva York","Ohio","Oklahoma","Oregón","Pensilvania","Rhode Island","Tennessee","Texas","Utah","Vermont","Virginia","Virginia Occidental","Washington","Wisconsin","Wyoming"],
-  "Canadá": ["Alberta","Columbia Británica","Manitoba","Nuevo Brunswick","Terranova y Labrador","Nueva Escocia","Ontario","Isla del Príncipe Eduardo","Quebec","Saskatchewan","Yukón","Territorios del Noroeste","Nunavut"],
-  "Australia": ["Nueva Gales del Sur","Victoria","Queensland","Australia Meridional","Australia Occidental","Tasmania","Territorio del Norte","Territorio de la Capital Australiana"]
+
+  "Portugal": [
+    "Lisboa", "Porto", "Braga", "Aveiro", "Coímbra", "Faro", "Setúbal",
+    "Madeira", "Azores"
+  ],
+
+  "Francia": [
+    "Île-de-France", "Occitanie", "Nouvelle-Aquitaine",
+    "Provence-Alpes-Côte d’Azur", "Auvergne-Rhône-Alpes",
+    "Hauts-de-France", "Normandie", "Bretagne", "Pays de la Loire"
+  ],
+
+  "Italia": [
+    "Lombardia", "Lazio", "Campania", "Sicilia", "Piemonte", "Veneto",
+    "Emilia-Romagna", "Toscana", "Puglia", "Sardegna"
+  ],
+
+  "Alemania": [
+    "Baviera", "Berlín", "Hamburgo", "Baden-Württemberg",
+    "Renania del Norte-Westfalia", "Hesse", "Sajonia", "Baja Sajonia",
+    "Brandeburgo", "Turingia"
+  ],
+
+  "Reino Unido": [
+    "England", "Scotland", "Wales", "Northern Ireland"
+  ],
+
+  "Estados Unidos": [
+    "California", "Texas", "New York", "Florida", "Illinois", "Pennsylvania",
+    "Ohio", "Georgia", "North Carolina", "Michigan", "Washington", "Arizona"
+  ],
+
+  "Canadá": [
+    "Ontario", "Quebec", "British Columbia", "Alberta",
+    "Manitoba", "Saskatchewan", "Nova Scotia", "New Brunswick",
+    "Newfoundland and Labrador", "Prince Edward Island"
+  ]
 };
+
+// === Supabase ===
+export const SUPABASE_URL = "https://<TU-PROJECT>.supabase.co";
+export const SUPABASE_ANON_KEY = "<TU-ANON-KEY>";
