f5aa14eb35b79e6302759abdf6617472b8a7ff91
Du4lc <soydu4lc@gmail.com>
2025-08-16 11:24:42 +0200

    changes

--- supabase/05_seed.sql
+++
@@ -1,7 +0,0 @@
--- Poblado inicial de sectores
-insert into public.tags (slug, name) values
-  ('fotovoltaica','Fotovoltaica'),
-  ('farmaceutica','Farmacéutica'),
-  ('biomedicina','Biomedicina'),
-  ('ia','IA')
-on conflict do nothing;
