360757ec46c0cb62293a49925c2f98be7356646f
Du4lc <soydu4lc@gmail.com>
2025-08-23 12:32:47 +0200

    changes v2.2 sectors

---
+++ sectors.js
@@ -0,0 +1,172 @@
+// sectors.js – lista simplificada CNAE 2025 (nombres claros y concisos)
+window.sectorData = {
+  "Agricultura, ganadería y pesca": [
+    "Cultivos",
+    "Ganadería",
+    "Acuicultura",
+    "Silvicultura",
+    "Pesca",
+    "Servicios agrarios"
+  ],
+  "Industrias extractivas": [
+    "Petróleo y gas",
+    "Minerales metálicos",
+    "Minerales no metálicos"
+  ],
+  "Manufactura": [
+    "Alimentación",
+    "Bebidas",
+    "Madera",
+    "Papel",
+    "Impresión",
+    "Textil",
+    "Química",
+    "Farmacéutica",
+    "Plásticos",
+    "Materiales construcción",
+    "Metalurgia",
+    "Electrónica",
+    "Eléctrico",
+    "Maquinaria",
+    "Automoción",
+    "Transporte avanzado",
+    "Muebles",
+    "Instrumentos médicos",
+    "Reparación maquinaria"
+  ],
+  "Energía": [
+    "Solar",
+    "Eólica",
+    "Hidráulica",
+    "Biomasa",
+    "Nuclear",
+    "Gas",
+    "Hidrógeno",
+    "Almacenamiento",
+    "Distribución",
+    "Comercialización"
+  ],
+  "Agua y residuos": [
+    "Agua",
+    "Aguas residuales",
+    "Reciclaje",
+    "Valorización residuos",
+    "Gestión ambiental"
+  ],
+  "Construcción": [
+    "Edificación",
+    "Ingeniería civil",
+    "Reformas"
+  ],
+  "Comercio": [
+    "Mayorista alimentos",
+    "Mayorista industrial",
+    "Minorista alimentos",
+    "Minorista consumo",
+    "Vehículos",
+    "Combustibles",
+    "Comercio electrónico"
+  ],
+  "Transporte y logística": [
+    "Terrestre",
+    "Marítimo",
+    "Aéreo",
+    "Tubería",
+    "Almacenamiento",
+    "Mensajería",
+    "Logística",
+    "VTC",
+    "Teleféricos"
+  ],
+  "Hostelería": [
+    "Hoteles",
+    "Restauración",
+    "Puestos comida",
+    "Catering"
+  ],
+  "Comunicación y tecnología": [
+    "Edición",
+    "Audiovisual",
+    "Telecom",
+    "Software",
+    "Consultoría IT",
+    "Data centers",
+    "Ciberseguridad",
+    "Cloud",
+    "Procesamiento datos",
+    "Plataformas digitales",
+    "Medios",
+    "Videojuegos",
+    "Inteligencia artificial"
+  ],
+  "Finanzas": [
+    "Banca",
+    "Fintech",
+    "Seguros",
+    "Capital riesgo",
+    "Inversión",
+    "Holdings"
+  ],
+  "Inmobiliario": [
+    "Promoción",
+    "Proptech",
+    "Intermediación"
+  ],
+  "Servicios empresariales": [
+    "Alquiler",
+    "Empleo",
+    "Seguridad",
+    "Limpieza",
+    "Jardinería",
+    "Call center",
+    "Ferias",
+    "Apoyo empresas"
+  ],
+  "Profesionales y técnicos": [
+    "Jurídico",
+    "Contable",
+    "Consultoría",
+    "Consultoría tech",
+    "Ingeniería",
+    "I+D",
+    "Marketing",
+    "Comunicación",
+    "Diseño",
+    "Traducción"
+  ],
+  "Educación": [
+    "Primaria",
+    "Secundaria",
+    "Profesional",
+    "Superior",
+    "Educación especial",
+    "Online"
+  ],
+  "Sanidad y social": [
+    "Sanidad",
+    "Biomedicina",
+    "Salud digital",
+    "Servicios sociales",
+    "Servicios sanitarios"
+  ],
+  "Cultura y ocio": [
+    "Artes",
+    "Cine",
+    "Música",
+    "Deportes",
+    "Fitness",
+    "Turismo",
+    "Ocio nocturno",
+    "Recreación",
+    "Museos"
+  ],
+  "Otros servicios": [
+    "Asociaciones",
+    "ONGs",
+    "Sindicatos",
+    "Servicios personales",
+    "Reparación vehículos",
+    "Reparación informática",
+    "Funerarios"
+  ]
+};
